c2b3b1e1363364c6c3192f8ae3220dad4f26344a
Sed9yDataBank <benzidsedki@gmail.com>
2020-04-05 14:05:15 +0100

    Adding JPA Auditing Annotations To Main

--- src/main/java/com/kanbanedchain/lianatasks/LianatasksApplication.java
+++ src/main/java/com/kanbanedchain/lianatasks/LianatasksApplication.java
@@ -2,8 +2,10 @@ package com.kanbanedchain.lianatasks;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class LianatasksApplication {
 
 	public static void main(String[] args) {
